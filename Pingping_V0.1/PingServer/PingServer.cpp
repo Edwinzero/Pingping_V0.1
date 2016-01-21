916a87cd97d6341c4cb7fbce247b5cf2a117a570
Shang <zhaoxiaoshang@gmail.com>
2016-01-21 12:26:39 -0500

    add files

---
+++ Pingping_V0.1/PingServer/PingServer.cpp
@@ -0,0 +1,21 @@
+#if 1
+#define _WIN32_WINNT 0x0501
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <set>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <boost\thread.hpp>
+#include <Windows.h>
+
+using boost::asio::ip::tcp;
+
+int main() {
+
+}
+#endif
